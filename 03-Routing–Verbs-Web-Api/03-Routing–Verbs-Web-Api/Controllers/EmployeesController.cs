9e632bc75278c4bcb913163ab7b448d65eb3d7f8
edgarozorio94@gmail.com <edgarozorio94@gmail.com>
2024-08-21 17:56:04 -0400

    ejercicio 03

---
+++ 03-Routing–Verbs-Web-Api/03-Routing–Verbs-Web-Api/Controllers/EmployeesController.cs
@@ -0,0 +1,46 @@
+﻿using _03_Routing_Verbs_Web_Api.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_Routing_Verbs_Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        List<EmployeeDTO> employees = new List<EmployeeDTO>(); 
+        public EmployeesController()
+        {
+            employees.Add(new EmployeeDTO {Id = 1, Name = "Edgar", Position = "Programador" });
+            employees.Add(new EmployeeDTO {Id = 2, Name = "Javier", Position = "Mecanico" });
+            employees.Add(new EmployeeDTO {Id = 3, Name = "Goku", Position = "Sayayin" });
+        }
+
+
+        [HttpGet("GetEmployees")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(employees);
+
+        }
+        //[HttpGet("GetEmployee")] query params
+        //[HttpGet("GetEmployee/{id}/{name}")] parametros separados por /
+        [HttpGet("GetEmployees/{id}")]
+        public IActionResult GetEmployees(int id)
+        {
+            var employee = employees.Find(x=> x.Id == id);
+
+            return Ok(employee);
+
+        }   
+        
+        [HttpPost("CreateEmployees")]
+        public IActionResult CreateEmployees([FromBody] EmployeeDTO employee)
+        {
+            employees.Add(employee);
+
+            return Ok(employee);
+
+        }
+    }
+}
